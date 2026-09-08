aad059471d7d356f04c998ceaef1e52fc12eb56c
hemanth09-blk <hemanthchamarthi09@gmail.com>
2026-09-08 12:02:22 +0530

    Add employee API views

---
+++ employees/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("employees/", views.employee_list, name="employee-list"),
+    path("employees/<int:id>/", views.employee_detail, name="employee-detail"),
+]
