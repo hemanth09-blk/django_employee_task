fe8e436d9088c030033c51c78f82df50197109ab
hemanth09-blk <hemanthchamarthi09@gmail.com>
2026-09-10 16:45:56 +0530

    feat: implement employee crud and admin

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Employee
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        "employee_code",
+        "first_name",
+        "last_name",
+        "email",
+        "department",
+        "designation",
+        "salary",
+        "joining_date",
+        "is_active",
+    )
+    search_fields = (
+        "employee_code",
+        "first_name",
+        "last_name",
+        "email",
+        "department",
+    )
+    list_filter = (
+        "department",
+        "designation",
+        "is_active",
+    )
+    ordering = ("employee_code",)
